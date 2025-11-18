3cd5af4d730523f914b1732ef17aaf2e3518899b
Pranav Pise <pranav.pise@ncircletech.com>
2025-11-18 17:53:55 +0530

    Initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,9 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -17,17 +17,38 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
-        name="index"
+        name='(popular)'
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Popular",
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="explore" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name='(search)'
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Search",
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="search" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name='(addnew)'
+        options={{
+          title: "Addnew",
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="add-circle" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name='(activity)'
+        options={{
+          title: "Activity",
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="flash-on" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name='(profile)'
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="manage-accounts" color={color} />,
         }}
       />
     </Tabs>
